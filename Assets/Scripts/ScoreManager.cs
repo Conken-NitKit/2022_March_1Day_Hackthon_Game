ea13b343b6581325230d534cfd1c690c948e5050
yuriyuri1704 <haruonma1117@gmail.com>
2022-03-26 12:57:36 +0900

    変更：ScoreManagerスクリプト

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// スコアに関するクラス
+/// </summary>
 public class ScoreManager : MonoBehaviour
 {
     /// <summary>
     /// スコアの合計値を格納する変数
     /// </summary>
     public int totalScore = 0;
-    public Text scoreText;
+    [SerializeField] Text scoreText;
 
+    /// <summary>
+    /// 入力したスコア値を合計スコア変数に加算するメソッド
+    /// </summary>
     public void DisplayScore(int scoreValue = 1)
     {
         totalScore += scoreValue;
