a4ce8620dadc989467e564ee96a881a82e2fb590
woods-one <0820ricer@gmail.com>
2022-03-29 18:26:09 +0900

    追加: コメントアウトの追加

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// カメラの管理クラス
+/// </summary>
 public class CameraManager : MonoBehaviour
 {
+    private float blockScaleY = 1f;
+
+    /// <summary>
+    /// 呼び出すと1ブロック分カメラを移動するメソッド
+    /// </summary>
     public void MoveUpPosition()
     {
-        this.transform.position += new Vector3(0f, 1f, 0f);
+        this.transform.position += new Vector3(0f, blockScaleY, 0f);
     }
 }
