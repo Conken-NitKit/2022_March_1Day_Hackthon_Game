4c1feceddc88df1aedc6b2770c816ca76ac887aa
woods-one <0820ricer@gmail.com>
2022-04-02 23:28:17 +0900

    追加:背景の追加

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    public void GenerateBackGround()
+    {
+
+    }
+}
