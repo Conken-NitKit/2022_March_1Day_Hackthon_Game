eb622a8dba411beb639ccdda9b756ad9be41c4ba
woods-one <0820ricer@gmail.com>
2022-04-03 16:08:19 +0900

    変更: 上に行けば行くほど背景が変わるように変更

--- Assets/Scripts/BackGround.cs
+++ Assets/Scripts/BackGround.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class BackGround : MonoBehaviour
 {
-    public void GenerateBackGround()
-    {
+    [SerializeField]
+    Renderer[] backGroundRenderers;
 
+    int backGroundIndex;
+
+    private void Start()
+    {
+        backGroundIndex = 0;
+    }
+
+    public void MakeTransparentBackGround()
+    {
+        if(backGroundIndex == backGroundRenderers.Length - 1)
+        {
+            return;
+        }
+
+        backGroundRenderers[backGroundIndex].material.color -= new Color32(0, 0, 0, 5);
+
+        if(backGroundRenderers[backGroundIndex].material.color.a == 0)
+        {
+            backGroundIndex++;
+        }
     }
 }
